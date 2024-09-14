ecf8f3ab0b05cf583388c9d42d0eca84447f66c7
Alok <alokgithub@gmail.com>
2024-09-14 16:36:00 +0800

    Add smooth scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html style={{scrollBehavior: "smooth"}} lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
